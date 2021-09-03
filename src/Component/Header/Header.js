17cccc3c54c1304e6fab32c836232db7e39cb97e
Saurabh rana <ranasaurabh455@gmail.com>
2021-09-03 12:30:59 +0530

    make some changes

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -15,8 +15,8 @@ const Header = () => {
             <div className='para'>
                 <p>
                   <h3><strong>🖐 Hey,</strong></h3>
-                   <h3><strong>I,M Saurabh , a web developer.</strong></h3>
-                    <strong> <Typography>A Web Developer from hils,with a flair for design, devoted to building quality websites that are easy to maintain and scalable.</Typography> </strong>
+                   <h3><strong>I'M Saurabh , a web developer.</strong></h3>
+                    <strong> <Typography>A Web Developer from hills,with a flair for design, devoted to building quality websites that are easy to maintain and scalable.</Typography> </strong>
                 </p>
                 <button type="button" className="btn btn-outline-warning search" style={{'margin-bottom':'2vh'}} onClick={()=>{window.location.assign('https://www.instagram.com/saurabhranaaaa__/')}}>Know More</button>
             </div>
